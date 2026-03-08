e0f5c0794504cc1bb249002c9cad800720ac78fc
Uğur Umut <ugur.34.umut@gmail.com>
2026-03-08 10:23:23 +0300

    İlk kayıt

---
+++ hesapla.py
@@ -0,0 +1,9 @@
+# Kullanıcıdan iki sayı alalım
+sayi1 = float(input("Birinci sayıyı gir: "))
+sayi2 = float(input("İkinci sayıyı gir: "))
+
+# Toplama işlemi yapalım
+toplam = sayi1 + sayi2
+
+# Sonucu ekrana yazdıralım
+print("İki sayının toplamı:", toplam)
